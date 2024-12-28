1ed70979bf0553d5c1b6ac44b9ec5825d7aad0cf
Willi <willi_8_7@gmx.de>
2024-12-28 12:17:51 +0100

    Improve styling of buttons

--- gameproject/scenes/battle/battle_manager.gd
+++ gameproject/scenes/battle/battle_manager.gd
@@ -1,6 +1,11 @@
 extends Control
 
 @onready var attack_button : Button = $MenuBox/ButtonBox/LeftSide/AttackButton
+@onready var skill_button : Button = $MenuBox/ButtonBox/RightSide/SkillButton
+@onready var item_button : Button = $MenuBox/ButtonBox/LeftSide/ItemButton
+@onready var neg_button : Button = $MenuBox/ButtonBox/RightSide/NegButton
+
+@onready var info_text : Label = $MenuBox/PanelContainer/InfoText
 
 var all_battlers = []
 var player_battlers = []
@@ -21,6 +26,10 @@ func _ready() -> void:
 	all_battlers.sort_custom(func(a, b): return a.entity.agility > b.entity.agility)
 	
 	attack_button.pressed.connect(_perform_attack)
+	skill_button.pressed.connect(_choose_skill)
+	item_button.pressed.connect(_choose_item)
+	neg_button.pressed.connect(_choose_neg_target)
+	
 	
 	for p in player_battlers:
 		p.entity.turn_ended.connect(_next_turn)
@@ -40,6 +49,19 @@ func _process(delta: float) -> void:
 func _perform_attack() -> void:
 	var target = enemy_battlers[selected_target]
 	current_turn.entity.start_attacking(target)
+	
+	
+func _choose_skill() -> void:
+	print("Choosing a skill")
+	
+	
+	
+func _choose_item() -> void:
+	print("Choosing an item")
+	
+	
+func _choose_neg_target() -> void:
+	print("Choose a Negotiation Partner")
 	
 	
 func _attack_random_battler() -> void:
@@ -71,3 +93,35 @@ func _update_turn() -> void:
 
 func has_battle_ended() -> bool:
 	return false
+
+
+func _on_attack_button_mouse_entered() -> void:
+	info_text.text = "Attack the target"
+
+
+func _on_attack_button_mouse_exited() -> void:
+	info_text.text = "Choose your next action"
+
+
+func _on_skill_button_mouse_entered() -> void:
+	info_text.text = "Use a Skill"
+
+
+func _on_skill_button_mouse_exited() -> void:
+	info_text.text = "Choose your next action"
+	
+	
+func _on_item_button_mouse_entered() -> void:
+	info_text.text = "Use Item"
+
+
+func _on_item_button_mouse_exited() -> void:
+	info_text.text = "Choose your next action"
+	
+	
+func _on_neg_button_mouse_entered() -> void:
+	info_text.text = "Start Negotiation with a Monster"
+
+
+func _on_neg_button_mouse_exited() -> void:
+	info_text.text = "Choose your next action"
