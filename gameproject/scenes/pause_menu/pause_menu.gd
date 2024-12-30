2d3903c5b71b6352d25f14ae1b44ab2b22aee17f
Anton Huber <anton.huber98@yahoo.com>
2024-12-30 12:18:36 +0100

    fix pause menu buttons

--- gameproject/scenes/pause_menu/pause_menu.gd
+++ gameproject/scenes/pause_menu/pause_menu.gd
@@ -1,14 +1,28 @@
 extends Control
 
+@onready var resumeBtn : Button = $PanelContainer/VBoxContainer/Resume
+@onready var restartBtn : Button = $PanelContainer/VBoxContainer/Restart
+@onready var quitBtn : Button = $PanelContainer/VBoxContainer/Quit
+
+
 func _ready():
 	$AnimationPlayer.play("RESET")
 
+func changeBtnStates(disabled):
+	resumeBtn.disabled = disabled
+	restartBtn.disabled = disabled
+	quitBtn.disabled = disabled
+
 func resume():
 	get_tree().paused = false
+	changeBtnStates(true)
+	
 	$AnimationPlayer.play_backwards("blur")
 	
 func pause():
 	get_tree().paused = true	
+	changeBtnStates(false)
+
 	$AnimationPlayer.play("blur")
 
 func testEsc():
