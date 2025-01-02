4796ed0076d2cc10349c933fa7475552e28b9c7f
Anton Huber <anton.huber98@yahoo.com>
2025-01-02 12:48:49 +0100

    add logic for map leading to battle

--- gameproject/scenes/overworld/Event.gd
+++ gameproject/scenes/overworld/Event.gd
@@ -1,24 +1,41 @@
 extends Node2D
 
+@onready var button : TextureButton = $Button
+
+
 const margin = 5
 
 var children: Array = []
 
+func _ready() -> void:
+	var textures = [
+		preload("res://imgs/overworld_icons/coin.png"),
+		preload("res://imgs/overworld_icons/battle_icon.png"),
+		preload("res://imgs/overworld_icons/question_mark_icon.png")
+	]
+	
+	var random_index = randi() % textures.size()  # Get a random index between 0 and 2
+	button.texture_normal = textures[random_index]  # Assign the selected texture to the button
+
+	
 func add_child_event(child):
 	if !children.has(child):
 		children.append(child)
 		queue_redraw()
 
 func _draw():
-	draw_circle(Vector2.ZERO, 4, Color.WHITE_SMOKE)
-	
+
 	for child in children:
 		var line = child.position - position
+		
 		var normal = line.normalized()
 		line -= margin * normal
 		var color = Color.GRAY
+		
+		#print(normal * margin, line)
+		
 		draw_line(normal * margin, line, color, 2, true)
 
 
 func _on_button_pressed() -> void:
-	print("Button pressed!")
+	get_tree().change_scene_to_file("res://scenes/battle/battle.tscn")
