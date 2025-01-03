acf5764c5fb5d3c7f8678f8cd7bdbbb54f068f11
Anton Huber <anton.huber98@yahoo.com>
2025-01-03 11:56:32 +0100

    Remove print statements

--- gameproject/scenes/overworld/overworld.gd
+++ gameproject/scenes/overworld/overworld.gd
@@ -1,8 +1,6 @@
 extends Control
 
-
 @onready var mapTexture: TextureRect = $TextureRect
-
 
 var events = {}
 var event_scene = preload("res://scenes/overworld/Event.tscn")
@@ -13,8 +11,8 @@ var scale_factor = 80
 var map_scale = 65
 var scale_diff = scale_factor - map_scale
 
-const plane_height = 25 # Scaled up this means 2000
-const plane_width = 16 # Scaled up this means 1280
+const plane_height = 25 # Scaled up by 80: 2000 / by 65: 1625
+const plane_width = 16 # Scaled up by 80: 1280 / by 65:  1040
 const node_count = plane_height * plane_width / 9
 const path_count = 6
 
@@ -27,9 +25,7 @@ func _ready() -> void:
 	var paths = map_data[1]
 	
 	map_root_position = mapTexture.position
-	print(mapTexture.position)
-	print(mapTexture.size)
-
+	
 	map_root_position.x += (scale_diff * plane_width / 2)
 	map_root_position.y += (scale_diff * plane_height / 2)
 	
@@ -39,8 +35,6 @@ func _ready() -> void:
 	
 		
 		event.position = point * map_scale + map_root_position
-		
-		print(event.position)
 		
 		add_child(event)
 		events[k] = event
