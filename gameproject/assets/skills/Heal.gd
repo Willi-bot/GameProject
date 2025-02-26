ced10f67e45821af47c894bf5b355fe190e2b47d
Willi <willi_8_7@gmx.de>
2025-02-26 11:31:35 +0100

    Add Heal Animation

--- gameproject/assets/skills/Heal.gd
+++ gameproject/assets/skills/Heal.gd
@@ -1,16 +1,37 @@
 class_name Heal
 extends Skill
 
+
+var animation: PackedScene
+var animation_scene: AnimatedSprite2D
+var timer: Timer
+var target
+
 func _init():
 	super._init()
 	
 	name = "Heal"
 	description = "Heal za ally :DDD"
+	animation = preload("res://textures/animations/heal_animation.tscn")
 
 
 func execute(entity: BaseEntity) -> void:
 	entity.use_mp(mp_cost)
-	var target = await Global.bm.get_player_target()
+	target = await Global.bm.get_player_target()
 	var heal_amount = entity.intelligence * 2
 	target.entity.heal(heal_amount)
+	animation_scene = animation.instantiate() as AnimatedSprite2D
+	target.add_child(animation_scene)
+	animation_scene.play()
+	
+	timer = Timer.new()
+	timer.wait_time = 0.75
+	timer.timeout.connect(timer_timeout)
+	target.add_child(timer)
+	timer.start()
+
+
+func timer_timeout() -> void:
+	target.remove_child(animation_scene)
+	target.remove_child(timer)
 	turn_ended.emit()
